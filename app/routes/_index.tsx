dc60137d5bab725e6d4eec42904f07355e5f7d3a
Daniel Van Cuylenburg <email@danielvanc.com>
2024-03-17 21:50:34 +0000

    feat: experiment with theme switching

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,44 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form, json, useActionData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Baseline" },
+    { name: "description", content: "Welcome to Baseline!" },
   ];
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const data = await request.formData();
+  const team = data.get("team-chooser");
+
+  return json({ team });
+}
+
 export default function Index() {
+  const data = useActionData<typeof action>();
+  const team = data?.team ?? null;
+
   return (
-    <div className="p-10">
-      <h1>(Staging) New NBA Stats app coming soon...</h1>
-    </div>
+    <>
+      <main>
+        <div className="p-11 bg-primary" data-theme={team}>
+          <h1>Primary theme</h1>
+        </div>
+        {/* <div className="p-11 bg-primary" data-theme="rainforest">
+          <h1>Rainforest theme</h1>
+        </div>
+        <div className="p-11 bg-primary" data-theme="candy">
+          <h1>Candy theme</h1>
+        </div> */}
+      </main>
+      <aside>
+        <Form method="POST">
+          <button type="submit" value="phx" name="team-chooser">
+            Choose PHX
+          </button>
+        </Form>
+      </aside>
+    </>
   );
 }
