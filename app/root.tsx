f018bbf3f83a297a9057a82ef5cf7684baef1def
Daniel Van Cuylenburg <email@danielvanc.com>
2024-03-18 22:22:37 +0000

    refactor: move fetcher to root and add css vars

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,32 @@
-import { LinksFunction } from "@remix-run/node";
+import { ActionFunctionArgs, json, LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useFetcher,
 } from "@remix-run/react";
-
 import tailwindStyles from "~/styles/tailwind.css?url";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStyles }].filter(Boolean);
 };
 
+export async function action({ request }: ActionFunctionArgs) {
+  const data = await request.formData();
+  const team = data.get("team-chooser");
+
+  return json({ team });
+}
+
 export default function App() {
+  const teamFetcher = useFetcher<typeof action>();
+  const hasSelectedTeam = !!(
+    teamFetcher.state === "idle" && teamFetcher?.data?.team
+  );
+  const team = hasSelectedTeam ? teamFetcher?.data?.team : null;
+
   return (
     <html lang="en">
       <head>
@@ -22,8 +35,22 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className="bg-brand-bg border-l-brand-bdr" data-theme={team}>
+        <header>
+          <nav>
+            <h1>Baseline</h1>
+            <div>
+              <teamFetcher.Form method="post">
+                <button type="submit" value="phx" name="team-chooser">
+                  Choose PHX
+                </button>
+              </teamFetcher.Form>
+            </div>
+          </nav>
+        </header>
+        <main>
+          <Outlet />
+        </main>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,4 @@
-import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
-import { json, useFetcher } from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,32 +7,10 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function action({ request }: ActionFunctionArgs) {
-  const data = await request.formData();
-  const team = data.get("team-chooser");
-
-  return json({ team });
-}
-
 export default function Index() {
-  const fetcher = useFetcher<typeof action>();
-  const hasSelectedTeam = !!(fetcher.state === "idle" && fetcher?.data?.team);
-  const team = hasSelectedTeam ? fetcher?.data?.team : null;
-
   return (
-    <>
-      <main>
-        <div className="p-11 bg-primary text-brand" data-theme={team}>
-          <h1>Primary theme</h1>
-        </div>
-      </main>
-      <aside>
-        <fetcher.Form method="post">
-          <button type="submit" value="phx" name="team-chooser">
-            Choose PHX
-          </button>
-        </fetcher.Form>
-      </aside>
-    </>
+    <div className="p-11">
+      <h1>Primary theme</h1>
+    </div>
   );
 }
