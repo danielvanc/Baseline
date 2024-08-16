e680b653db482d21ba4d5e41755fff85566a612a
Daniel Van Cuylenburg <email@danielvanc.com>
2024-08-16 14:55:40 +0100

    chore: updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import tailwindcss from "@tailwindcss/vite";
+
 import { defineConfig } from "vite";
 import { installGlobals } from "@remix-run/node";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -11,7 +11,6 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    tailwindcss(),
     remix({
       ignoredRouteFiles: ["**/*.css"],
     }),
