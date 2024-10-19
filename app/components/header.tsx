183ca3104dc0ded6e0f0005c7a8da2e8158337fc
Daniel Van Cuylenburg <email@danielvanc.com>
2024-10-19 11:33:34 +0100

    feat: update nav

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -32,73 +32,81 @@ export default function Header({
           <span className="underline">Base</span>Line
         </div>
         <div className="pt-1 relative">
-          <Form method="post" id="team-form">
-            <Listbox
-              defaultValue={fallbackValue}
-              value={selected}
-              onChange={(selected) => {
-                const selectedTeam = {
-                  team: selected,
-                };
-                setSelected(selected);
-                submitTeamSelection(selectedTeam, {
-                  method: "post",
-                });
-              }}
-            >
-              <div className="relative mt-2 w-[215px]">
-                <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  <span ref={spanRef} className="hidden">
-                    {selected ?? "All teams"}
-                  </span>
-                  <span className="block truncate">
-                    {teams?.find((team) => team.abbr === selected)?.name ??
-                      "All teams"}
-                  </span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <ChevronUpDownIcon
-                      aria-hidden="true"
-                      className="h-5 w-5 text-gray-400"
-                    />
-                  </span>
-                </ListboxButton>
-
-                <ListboxOptions
-                  transition
-                  className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
-                >
-                  <ListboxOption
-                    key={`theme-no-team`}
-                    value={defaultTheme}
-                    className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+          <nav>
+            <ul className="flex items-center text-white space-x-8 leading-9">
+              <li>Schedule</li>
+              <li>Results</li>
+              <li className="w-[215px]">
+                <Form method="post" id="team-form">
+                  <Listbox
+                    defaultValue={fallbackValue}
+                    value={selected}
+                    onChange={(selected) => {
+                      const selectedTeam = {
+                        team: selected,
+                      };
+                      setSelected(selected);
+                      submitTeamSelection(selectedTeam, {
+                        method: "post",
+                      });
+                    }}
                   >
-                    <span className="block truncate font-normal group-data-[selected]:font-semibold">
-                      All teams
-                    </span>
+                    <div className="relative mt-2">
+                      <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        <span ref={spanRef} className="hidden">
+                          {selected ?? "All teams"}
+                        </span>
+                        <span className="block truncate">
+                          {teams?.find((team) => team.abbr === selected)
+                            ?.name ?? "All teams"}
+                        </span>
+                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                          <ChevronUpDownIcon
+                            aria-hidden="true"
+                            className="h-5 w-5 text-gray-400"
+                          />
+                        </span>
+                      </ListboxButton>
 
-                    <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
-                      <CheckIcon aria-hidden="true" className="h-5 w-5" />
-                    </span>
-                  </ListboxOption>
-                  {teams.map((team) => (
-                    <ListboxOption
-                      key={`theme-${team.abbr}`}
-                      value={team.abbr}
-                      className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
-                    >
-                      <span className="block truncate font-normal group-data-[selected]:font-semibold">
-                        {team.name}
-                      </span>
+                      <ListboxOptions
+                        transition
+                        className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+                      >
+                        <ListboxOption
+                          key={`theme-no-team`}
+                          value={defaultTheme}
+                          className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+                        >
+                          <span className="block truncate font-normal group-data-[selected]:font-semibold">
+                            All teams
+                          </span>
 
-                      <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
-                        <CheckIcon aria-hidden="true" className="h-5 w-5" />
-                      </span>
-                    </ListboxOption>
-                  ))}
-                </ListboxOptions>
-              </div>
-            </Listbox>
-            {/* <select
+                          <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                            <CheckIcon aria-hidden="true" className="h-5 w-5" />
+                          </span>
+                        </ListboxOption>
+                        {teams.map((team) => (
+                          <ListboxOption
+                            key={`theme-${team.abbr}`}
+                            value={team.abbr}
+                            className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+                          >
+                            <span className="block truncate font-normal group-data-[selected]:font-semibold">
+                              {team.name}
+                            </span>
+
+                            <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                              <CheckIcon
+                                aria-hidden="true"
+                                className="h-5 w-5"
+                              />
+                            </span>
+                          </ListboxOption>
+                        ))}
+                      </ListboxOptions>
+                    </div>
+                  </Listbox>
+                  {/* <select
               name="team"
               onChange={(e) => submitTeamSelection(e.target.form)}
               ref={formRef}
@@ -113,7 +121,10 @@ export default function Header({
                 </option>
               ))}
             </select> */}
-          </Form>
+                </Form>
+              </li>
+            </ul>
+          </nav>
         </div>
       </nav>
     </header>
