dd82b12c77e55367b58cc29ee3bce4c339e308d5
Daniel Van Cuylenburg <email@danielvanc.com>
2024-11-17 10:58:10 +0000

    fix: use relative routing. remove fetch call

--- app/mocks/node.ts
+++ app/mocks/node.ts
@@ -3,7 +3,9 @@ import { handlers } from "./handlers";
 
 export const server = setupServer(...handlers);
 
-console.info("🔶 Mock server running");
+if (process.env.MOCKS === "true") {
+  console.info("🔶 Mock server running");
 
-process.once("SIGINT", () => server.close());
-process.once("SIGTERM", () => server.close());
+  process.once("SIGINT", () => server.close());
+  process.once("SIGTERM", () => server.close());
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,5 @@
-import { route, type RouteConfig } from "@remix-run/route-config";
+import { relative, type RouteConfig } from "@remix-run/route-config";
 
-export const routes: RouteConfig = [route("/", "routes/index.tsx")];
+const { route } = relative("./app/routes");
+
+export const routes: RouteConfig = [route("/", "index.tsx")];
