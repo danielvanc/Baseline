2908aa992f4914c423961f2998d1aac4ea65aa5b
Daniel Van Cuylenburg <email@danielvanc.com>
2025-01-12 10:45:27 +0000

    feat: add caching for theme switching

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/index";
-import { getGamesToday } from "~/utils/games";
+import { getGamesToday, type TodaysGames } from "~/utils/games";
 import React from "react";
-import { Await } from "react-router";
+import { Await, data } from "react-router";
 import SkeletonTodaysGames from "~/components/ui/loading/skeleton-todays-games";
 import LatestGames from "~/components/latestGames";
 
@@ -15,7 +15,33 @@ export function meta() {
 export async function loader() {
   const gamesData = getGamesToday();
 
+  return data(
+    { gamesData },
+    {
+      headers: {
+        "Cache-Control": "max-age=3600, public",
+      },
+    }
+  );
+}
+
+clientLoader.hydrate = true;
+
+let cache: { gamesData: TodaysGames };
+
+export async function clientLoader({ serverLoader }: Route.ClientLoaderArgs) {
+  if (cache) return { gamesData: cache.gamesData };
+
+  const { gamesData } = (await serverLoader()) as unknown as {
+    gamesData: TodaysGames;
+  };
+  cache = { gamesData };
+
   return { gamesData };
+}
+
+export function HydrateFallback() {
+  return <div>Loading...</div>;
 }
 
 export default function Index({
@@ -23,7 +49,7 @@ export default function Index({
 }: Route.ComponentProps) {
   return (
     <React.Suspense fallback={<SkeletonTodaysGames />}>
-      <Await resolve={gamesData}>
+      <Await resolve={gamesData as unknown as Promise<{ data: TodaysGames }>}>
         {(gamesData) => <LatestGames data={gamesData.data} />}
       </Await>
     </React.Suspense>
