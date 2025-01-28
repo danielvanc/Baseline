fe015d600ee0172e86864e98c615ecb467080e75
Daniel Van Cuylenburg <email@danielvanc.com>
2025-01-28 16:58:44 +0000

    feat: responsive styling

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/game";
 import { getCDNLogo, getGameStats, type GameStats } from "~/utils/games";
 import { format } from "date-fns";
+import { Link } from "react-router";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const { gameId, gameDate } = params;
@@ -21,32 +22,34 @@ export default function Game({
 
   return (
     <main className="max-w-[950px] mx-auto text-white pb-40">
-      <div className="max-w-[450px] mx-auto bg-white rounded-lg p-4 -bottom-16 relative text-gray-700 shadow-2xl">
+      <div className="max-w-[450px] mx-auto bg-white rounded-lg p-4 lg:-bottom-16 mb-5 lg:mb-0 relative text-gray-700 shadow-2xl">
         <p className="text-center opacity-25">
           {format(gameEt, "dd / MM / yyyy - hh:mm")} (ET)
         </p>
-        <div className="flex space-x-5 justify-between py-4">
-          <div className="flex  justify-between space-x-8 items-center w-1/2">
-            <span className="text-2xl text-right max-w-[160px] w-full">
-              <span className="max-w-[100px] block float-right">
+        <div className="md:flex md:space-x-5 justify-between py-4">
+          <div className="flex justify-between space-x-8 items-center md:w-1/2">
+            <span className="text-lg md:text-2xl text-center md:text-right md:max-w-[160px] w-full">
+              <span className="md:max-w-[100px] block md:float-right">
                 {awayTeamName}
               </span>
             </span>
-            <span className="text-lg">{awayTeam.score}</span>
           </div>
-          <div className="flex flex-row-reverse items-center w-1/2 justify-between">
-            <span className="ml-8 text-2xl max-w-[160px] w-full">
+          <div className="flex justify-center text-center space-x-5">
+            <span className="text-lg">{awayTeam.score}</span>
+            <span className="text-lg">{homeTeam.score}</span>
+          </div>
+          <div className="flex flex-row-reverse items-center md:w-1/2 justify-between text-center">
+            <span className="md:ml-8 text-lg md:text-2xl md:max-w-[160px] w-full">
               {homeTeamName}
             </span>
-            <span className="text-lg">{homeTeam.score}</span>
           </div>
         </div>
         <p className="text-center opacity-25">{gameLabel}</p>
       </div>
 
-      <div className="flex justify-between space-x-16 border-white border-[1px] rounded-md p-5 border-opacity-25">
-        <div className="w-1/2">
-          <div className="flex items-center mb-5">
+      <div className="md:flex justify-between md:space-x-16 border-white border-[1px] rounded-md p-5 border-opacity-25">
+        <div className="md:w-1/2 mb-12 md:mb-0">
+          <div className="flex items-center mb-5 space-x-3">
             <img
               src={getCDNLogo(awayTeam.teamId)}
               alt=""
@@ -58,7 +61,7 @@ export default function Game({
             </ul>
           </div>
           <h3 className="section-heading-small-upper">Score per period</h3>
-          <ul className="flex mb-16 space-x-7 mt-2">
+          <ul className="flex mb-8 md:mb-16 space-x-3 md:space-x-7 mt-2">
             {awayTeam.periods.map((period, index) => (
               <li
                 key={`${awayTeam.teamId}-${awayTeam.score}-${index}`}
@@ -72,7 +75,7 @@ export default function Game({
             ))}
           </ul>
 
-          <div className="mb-5 text-right">
+          <div className="mb-5 md:text-right">
             <h3 className="section-heading-small-upper">Game leader</h3>
             <ul className="[&>li]:opacity-60">
               <li>
@@ -99,12 +102,12 @@ export default function Game({
           </div>
         </div>
 
-        <div className="w-1/2">
-          <div className="flex flex-row-reverse items-center mb-5">
+        <div className="md:w-1/2 border-t-[1px] border-white border-opacity-25 md:border-t-0 pt-8 md:pt-0">
+          <div className="flex md:flex-row-reverse items-center mb-5">
             <img
               src={getCDNLogo(homeTeam.teamId)}
               alt=""
-              className="w-20 h-20 inline-block md:mr-2"
+              className="w-20 h-20 inline-block md:mr-2 md:ml-5"
             />
             <ul className="text-right">
               <li>Wins: {homeTeam.wins}</li>
@@ -112,7 +115,7 @@ export default function Game({
             </ul>
           </div>
           <h3 className="section-heading-small-upper">Score per period</h3>
-          <ul className="flex mb-16 space-x-7 mt-2">
+          <ul className="flex  mb-8 md:mb-16 space-x-3 md:space-x-7 mt-2">
             {homeTeam.periods.map((period, index) => (
               <li
                 key={`${homeTeam.teamId}-${homeTeam.score}-${index}`}
@@ -139,7 +142,7 @@ export default function Game({
             </ul>
           </div>
 
-          <div className="text-right">
+          <div className="md:text-right">
             <h3 className="section-heading-small-upper">Team leader</h3>
             <ul className="[&>li]:opacity-60">
               <li>
@@ -153,6 +156,10 @@ export default function Game({
           </div>
         </div>
       </div>
+
+      <p className="text-center mt-5">
+        <Link to="/">Home</Link>
+      </p>
     </main>
   );
 }
