f7ecbc9bb17aeca832188724a1cd028e0e51411b
Daniel Van Cuylenburg <email@danielvanc.com>
2025-01-30 19:57:38 +0000

    test: coverage for upcoming games

---
+++ app/__tests__/upcomingGames.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import UpcomingGames from "~/components/upcomingGames";
+import { getUpcomingGames } from "~/utils/games";
+
+async function setUp() {
+  const upcomingGames = await getUpcomingGames();
+
+  render(<UpcomingGames data={upcomingGames} theme="all" />);
+}
+
+test("Renders Upcoming games component successfully", async () => {
+  await setUp();
+
+  expect(
+    screen.getByRole("heading", {
+      name: /upcoming games/i,
+    })
+  ).toBeInTheDocument();
+});
+
+test("Renders correct amount of games", async () => {
+  await setUp();
+
+  const gamesList = screen.getByRole("list");
+  expect(gamesList).toBeInTheDocument();
+  expect(gamesList.children).toHaveLength(3);
+});
