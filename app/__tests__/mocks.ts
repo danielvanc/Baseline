0e17fa6bc952913a153e796fbf1f0e9628ec7345
Daniel Van Cuylenburg <email@danielvanc.com>
2025-01-30 19:00:00 +0000

    test: coverage for gameDateTest component

--- app/__tests__/mocks.ts
+++ app/__tests__/mocks.ts
@@ -65,6 +65,38 @@ export const gamesYesterdayNotStarted: TodaysGames = {
   ],
 };
 
+export const gamesTodayNotStarted: TodaysGames = {
+  gameDate: "2025-01-29T03:00:00Z",
+  games: [
+    {
+      gameId: "234234234242",
+      gameEt: "2025-01-29T03:00:00Z",
+      gameTimeUTC: "2025-01-29T03:00:00Z",
+      gameLabel: "Test Game",
+      gameSubLabel: "Test Sub Label",
+      gameStatusText: "",
+      awayTeam: {
+        losses: 1,
+        score: 112,
+        teamCity: "Charlotte",
+        teamId: 1610612766,
+        teamName: "Hornets",
+        teamTricode: "CHA",
+        wins: 2,
+      },
+      homeTeam: {
+        losses: 1,
+        score: 110,
+        teamCity: "Chicago",
+        teamId: 1610612741,
+        teamName: "Bulls",
+        teamTricode: "CHI",
+        wins: 2,
+      },
+    },
+  ],
+};
+
 export const latestNews: NewsItem[] = [
   {
     title: "Wemby gets a hero’s welcome in Paris",
