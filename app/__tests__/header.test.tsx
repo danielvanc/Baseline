80fb692ed6d27e6cd7482d1f5b53a6dce5b1cc63
Daniel Van Cuylenburg <email@danielvanc.com>
2025-01-30 16:48:25 +0000

    test: add coverage for header component

---
+++ app/__tests__/header.test.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import { createRoutesStub } from "react-router";
+import userEvent from "@testing-library/user-event";
+import Header from "~/components/header";
+
+beforeAll(() => {
+  global.ResizeObserver = class {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+});
+
+afterAll(() => {
+  vi.clearAllMocks();
+});
+
+beforeEach(() => {
+  const spanRef = { current: document.createElement("span") };
+
+  const HeaderStub = createRoutesStub([
+    {
+      path: "/",
+      Component: () => {
+        return <Header defaultTheme="all" newTheme={"all"} spanRef={spanRef} />;
+      },
+      HydrateFallback: () => null,
+    },
+  ]);
+
+  render(<HeaderStub />);
+});
+
+test("Renders Header component successfully", () => {
+  expect(screen.getByRole("banner")).toBeInTheDocument();
+  expect(screen.getAllByRole("navigation")[0]).toBeInTheDocument();
+  expect(screen.getAllByRole("navigation")[1]).toBeInTheDocument();
+
+  const logoLink = screen.getByRole("link", { name: /base line/i });
+  expect(logoLink).toBeInTheDocument();
+  expect(logoLink).toHaveAttribute("href", "/");
+});
+
+test("Renders dropdown successfully when clicked on", async () => {
+  const dropdown = screen.getByRole("button", { name: /all all teams/i });
+  expect(dropdown).toBeInTheDocument();
+
+  await userEvent.click(dropdown);
+
+  expect(screen.getByText("Phoenix Suns")).toBeInTheDocument();
+});
